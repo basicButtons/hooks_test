34d059d865fdd6a908095c9c96191ba86137b679
Maxuan <786609202@qq.com>
2022-02-22 13:50:17 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    console.log("123123");
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
